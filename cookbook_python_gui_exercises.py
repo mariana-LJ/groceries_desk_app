fba8e1ce9bb179c559e850aa7a0dd33b45248036
mariana-LJ <mariana.lopez.jaimez@gmail.com>
2016-05-09 17:28:20 -0700

    Adding changes not previously staged for the exercise file.

--- cookbook_python_gui_exercises.py
+++ cookbook_python_gui_exercises.py
@@ -1,218 +1,187 @@
 import tkinter as tk
+import Tooltip as tt
 from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import Menu
 from tkinter import messagebox as mBox
 from tkinter import Spinbox
 
-class ToolTip(object):
-    def __init__(self, widget):
-        self.widget = widget
-        self.tipwindow = None
-        self.id = None
-        self.x = self.y = 0
-    
-    def showtip(self, text):
-        "Display text in tooltip window"
-        self.text = text
-        if self.tipwindow or not self.text:
-            return
-        print(self.widget.bbox("insert"))
-        print(type(self.widget.bbox("insert")))
-        x, y, cx, cy = self.widget.bbox("insert").split()
-        x = int(x)
-        y = int(y)
-        cx = int(cx)
-        cy = int(cy)
-        x = x + self.widget.winfo_rootx() + 27
-        y = y + cy + self.widget.winfo_rooty() +27
-        self.tipwindow = tw = tk.Toplevel(self.widget)
-        tw.wm_overrideredirect(1)
-        tw.wm_geometry("+%d+%d" % (x, y))
 
-        label = tk.Label(tw, text=self.text, justify=tk.LEFT,
-                      background="#ffffe0", relief=tk.SOLID, borderwidth=1,
-                      font=("tahoma", "8", "normal"))
-        label.pack(ipadx=1)
-                
-    def hidetip(self):
-        tw =self.tipwindow
-        self.tipwindow = None
-        if tw:
-            tw.destroy()
+class OOP():
+    def __init__(self):
+        # Create instance
+        self.win = tk.Tk()
+        # Add a title
+        self.win.title("Python GUI")
+        self.createWidgets()
+
+    # Button Click event callback function
+    def clickMe(self):
+        self.action.configure(text='Hello ' + self.name.get()+ ' ' + self.numberChosen.get())
+        # aLabel.configure(foreground='red')
+
+    # Radiobutton callback
+    def radCall(self):
+        radSel = self.radVar.get()
+        if radSel == 0:
+            # win.configure(background=colors[0])
+            self.monty2.configure(text='Blue')
+        elif radSel == 1:
+            # win.configure(background=colors[1])
+            self.monty2.configure(text='Gold')
+        elif radSel == 2:
+            # win.configure(background=colors[2])
+            self.monty2.configure(text='Red')
+
+    # Spinbox callback
+    def _spin(self):
+        value = self.spin.get()
+        print(value)
+        self.scr.insert(tk.INSERT, value + '\n')
+
+    # Display a message box Callback function
+    def _msgBox(self):
+        # mBox.showinfo('Python Message Info Box', 'A Python GUI created using tkinter: \nThe Year is 2016.')
+        # Warning box
+        # mBox.showwarning('Python Message Warning Box', 'A Python GUI created using tkinter: \nWarning: There might be a bug in this code.')
+        # Error box
+        # mBox.showerror('Python Message Warning Box', 'A Python GUI created using tkinter:\nError: Houston we DO have a serious PROBLEM!')
+        answer = mBox.askyesno("Python Message Dual Choice Box", "Are you sure you really wish to do this?")
+        print(answer)
+
+    # Menu bar with functionality
+    def _quit(self):
+        self.win.quit()
+        self.win.destroy()
+        exit()
+
+    def createWidgets(self):
+        # Adding tabs
+        self.tabControl = ttk.Notebook(self.win)
+        self.tab1 = ttk.Frame(self.tabControl)
+        self.tabControl.add(self.tab1, text='Tab 1')
+        self.tabControl.pack(expand=1, fill="both")
+        self.tab2 = ttk.Frame(self.tabControl)
+        self.tabControl.add(self.tab2, text='Tab 2')
+
+        # Creating a container frame to hold all other widgets
+        self.monty = ttk.LabelFrame(self.tab1, text=' Monty Python ')
+        self.monty.grid(column=0, row=0, padx=8, pady=4)
+
+        # Second label for second tab
+        self.monty2 = ttk.LabelFrame(self.tab2, text=' The Snake ')
+        self.monty2.grid(column=0, row=0, padx=8, pady=4)
+        # win.resizable(0,0)
+        # Adding a label
+        self.aLabel = ttk.Label(self.monty, text="A label")
+        self.aLabel.grid(column=0, row=0)
 
 
-def createToolTip(widget, text):
-    toolTip = ToolTip(widget)
-    def enter(event):
-        toolTip.showtip(text)
-    def leave(event):
-        toolTip.hidetip()
-    widget.bind('<Enter>', enter)
-    widget.bind('<Leave>', leave)
+        # Adding a button
+        self.action = ttk.Button(self.monty, text="Click Me!", command=self.clickMe)
+        # Position button in second row, second column (zero-based)
+        self.action.grid(column=2, row=1)
 
-win = tk.Tk()
-win.title("Python GUI")
-# Adding tabs
-tabControl = ttk.Notebook(win)
-tab1 = ttk.Frame(tabControl)
-tabControl.add(tab1, text='Tab 1')
-tabControl.pack(expand=1, fill="both")
-tab2 = ttk.Frame(tabControl)
-tabControl.add(tab2, text='Tab 2')
+        # Changing label
+        ttk.Label(self.monty, text="Enter a name:").grid(column=0, row=0, sticky='W')
 
-# Creating a container frame to hold all other widgets
-monty = ttk.LabelFrame(tab1, text=' Monty Python ')
-monty.grid(column=0, row=0, padx=8, pady=4)
+        # Adding a textbox entry widget
+        self.name = tk.StringVar()
+        self.nameEntered = ttk.Entry(self.monty, width=12, textvariable=self.name)
+        self.nameEntered.grid(column=0, row=1, sticky=tk.W)
+        # Place cursor into name Entry
+        self.nameEntered.focus()
 
-# Second label for second tab
-monty2 = ttk.LabelFrame(tab2, text=' The Snake ')
-monty2.grid(column=0, row=0, padx=8, pady=4)
-# win.resizable(0,0)
-# Adding a label
-aLabel = ttk.Label(monty, text="A label")
-aLabel.grid(column=0, row=0)
+        # Adding combo box widgets
+        ttk.Label(self.monty, text="Choose a number: ").grid(column=1, row=0)
+        self.number = tk.StringVar()
+        self.numberChosen = ttk.Combobox(self.monty, width=12, textvariable=self.number, state='readonly')
+        self.numberChosen['values'] = (1, 2, 4, 42, 100)
+        self.numberChosen.grid(column=1, row=1)
+        self.numberChosen.current(0)
 
-# Button Click event callback function
-def clickMe():
-	action.configure(text='Hello ' + name.get()+ ' ' + numberChosen.get())
-	# aLabel.configure(foreground='red')
+        # Creating three checkbuttons
 
-# Adding a button
-action = ttk.Button(monty, text="Click Me!", command=clickMe)
-# Position button in second row, second column (zero-based)
-action.grid(column=2, row=1)
+        self.chVarDis = tk.IntVar()
+        self.check1 = tk.Checkbutton(self.monty2, text="Disabled", variable=self.chVarDis, state='disabled')
+        self.check1.select()
+        self.check1.grid(column=0, row=4, sticky=tk.W)
+        self.chVarUn = tk.IntVar()
+        self.check2 = tk.Checkbutton(self.monty2, text="UnChecked", variable=self.chVarUn)
+        self.check2.deselect()
+        self.check2.grid(column=1, row=4, sticky=tk.W)
+        self.chVarEn = tk.IntVar()
+        self.check3 = tk.Checkbutton(self.monty2, text="Enabled", variable=self.chVarEn)
+        self.check3.select()
+        self.check3.grid(column=2, row=4, sticky=tk.W)
 
-# Changing label
-ttk.Label(monty, text="Enter a name:").grid(column=0, row=0, sticky='W')
+        # Adding Radio buttons
+        self.colors = ["Blue", "Gold", "Red"]
 
-# Adding a textbox entry widget
-name = tk.StringVar()
-nameEntered = ttk.Entry(monty, width=12, textvariable=name)
-nameEntered.grid(column=0, row=1, sticky=tk.W)
-# Place cursor into name Entry
-nameEntered.focus()
+        # create three radiobuttons
+        self.radVar = tk.IntVar()
+        # Select non-existing index for radVar
+        self.radVar.set(99)
 
-# Adding combo box widgets
-ttk.Label(monty, text="Choose a number: ").grid(column=1, row=0)
-number = tk.StringVar()
-numberChosen = ttk.Combobox(monty, width=12, textvariable=number, state='readonly')
-numberChosen['values'] = (1, 2, 4, 42, 100)
-numberChosen.grid(column=1, row=1)
-numberChosen.current(0)
+        # Create three radiobutton widgets with a loop
+        for col in range(3):
+            curRad = 'rad' + str(col)
+            curRad = tk.Radiobutton(self.monty2, text=self.colors[col], variable=self.radVar, value=col, command=self.radCall)
+            curRad.grid(column=col, row=5, sticky=tk.W)
 
-# Creating three checkbuttons
+        # Adding a Spinbox widget
+        # spin = Spinbox(monty, from_=0, to=10, width=5, bd=8, command=_spin)
+        # Adding a Spinbox widget using a set of values
+        self.spin = Spinbox(self.monty, values=(1, 2, 4, 42, 100), width=5, bd=8, command=self._spin)
+        self.spin.grid(column=0, row=2)
+        # Adding a second Spinbox widget
+        self.spin2 = Spinbox(self.monty, values=(0, 50, 100), width=5, bd=8, relief=tk.RAISED, command=self._spin)
+        self.spin2.grid(column=1, row=2)
 
-chVarDis = tk.IntVar()
-check1 = tk.Checkbutton(monty2, text="Disabled", variable=chVarDis, state='disabled')
-check1.select()
-check1.grid(column=0, row=4, sticky=tk.W)
-chVarUn = tk.IntVar()
-check2 = tk.Checkbutton(monty2, text="UnChecked", variable=chVarUn)
-check2.deselect()
-check2.grid(column=1, row=4, sticky=tk.W)
-chVarEn = tk.IntVar()
-check3 = tk.Checkbutton(monty2, text="Enabled", variable=chVarEn)
-check3.select()
-check3.grid(column=2, row=4, sticky=tk.W)
+        # Add a tooltip
+        tt.createToolTip(self.spin, 'This is a Spin control.')
 
-# Adding Radio buttons
-colors = ["Blue", "Gold", "Red"]
+        # Using a sccrolled text control
+        self.scrolW = 30
+        self.scrolH = 3
+        self.scr = scrolledtext.ScrolledText(self.monty, width=self.scrolW, height=self.scrolH, wrap=tk.WORD)
+        self.scr.grid(column=0, sticky='WE', columnspan=3)
 
-# Radiobutton callback
-def radCall():
-	radSel = radVar.get()
-	if radSel == 0: 
-		# win.configure(background=colors[0])
-		monty2.configure(text='Blue')
-	elif radSel == 1:
-		# win.configure(background=colors[1])
-		monty2.configure(text='Gold')
-	elif radSel == 2: 
-		# win.configure(background=colors[2])
-		monty2.configure(text='Red')
+        # Create a container to hold labels
+        self.labelsFrame = ttk.LabelFrame(self.monty2, text=' Labels in a Frame ')
+        self.labelsFrame.grid(column=0, row=7, padx=20, pady=40)
 
-# create three radiobuttons
-radVar = tk.IntVar()
-# Select non-existing index for radVar
-radVar.set(99)
+        # Place labels into the container element
+        ttk.Label(self.labelsFrame, text="Label1").grid(column=0, row=0)
+        ttk.Label(self.labelsFrame, text="Label2").grid(column=0, row=1)
+        ttk.Label(self.labelsFrame, text="Label3").grid(column=0, row=2)
 
-# Create three radiobutton widgets with a loop
-for col in range(3):
-	curRad = 'rad' + str(col)
-	curRad = tk.Radiobutton(monty2, text=colors[col], variable=radVar, value=col, command=radCall)
-	curRad.grid(column=col, row=5, sticky=tk.W)
+        # Add space around labels inside labelFrame
+        for child in self.labelsFrame.winfo_children():
+            child.grid_configure(padx=8, pady=4)
 
-# Spinbox callback
-def _spin():
-    value = spin.get()
-    print(value)
-    scr.insert(tk.INSERT, value + '\n')
+        # Place cursor into name Entry
+        self.nameEntered.focus()
 
-# Adding a Spinbox widget
-# spin = Spinbox(monty, from_=0, to=10, width=5, bd=8, command=_spin)
-# Adding a Spinbox widget using a set of values
-spin = Spinbox(monty, values=(1, 2, 4, 42, 100), width=5, bd=8, command=_spin)
-spin.grid(column=0, row=2)
-# Adding a second Spinbox widget
-spin2 = Spinbox(monty, values=(0, 50, 100), width=5, bd=8, relief=tk.RAISED, command=_spin)
-spin2.grid(column=1, row=2)
+        # Create a menu
+        self.menuBar = Menu(self.win)
+        self.win.config(menu=self.menuBar)
+        self.fileMenu = Menu(self.menuBar, tearoff=0)
+        self.fileMenu.add_command(label="New")
+        self.fileMenu.add_separator()
+        self.fileMenu.add_command(label="Exit", command=self._quit)
+        self.menuBar.add_cascade(label="File", menu=self.fileMenu)
 
-# Add a tooltip
-createToolTip(spin, 'This is a Spin control.')
+        self.helpMenu = Menu(self.menuBar, tearoff=0)
+        self.helpMenu.add_command(label="About", command=self._msgBox)
+        self.menuBar.add_cascade(label="Help", menu=self.helpMenu)
 
-# Using a sccrolled text control
-scrolW = 30
-scrolH = 3
-scr = scrolledtext.ScrolledText(monty, width=scrolW, height=scrolH, wrap=tk.WORD)
-scr.grid(column=0, sticky='WE', columnspan=3)
+        # Change the main windows icon
+        # win.iconbitmap(r'/usr/bin/python3/icon/path')
 
-# Create a container to hold labels
-labelsFrame = ttk.LabelFrame(monty2, text=' Labels in a Frame ')
-labelsFrame.grid(column=0, row=7, padx=20, pady=40)
 
-# Place labels into the container element
-ttk.Label(labelsFrame, text="Label1").grid(column=0, row=0)
-ttk.Label(labelsFrame, text="Label2").grid(column=0, row=1)
-ttk.Label(labelsFrame, text="Label3").grid(column=0, row=2)
-
-# Add space around labels inside labelFrame
-for child in labelsFrame.winfo_children():
-	child.grid_configure(padx=8, pady=4)
-
-# Place cursor into name Entry
-nameEntered.focus()
-
-# Menu bar with functionality
-def _quit():
-	win.quit()
-	win.destroy()
-	exit()
-
-menuBar = Menu(win)
-win.config(menu=menuBar)
-fileMenu = Menu(menuBar, tearoff=0)
-fileMenu.add_command(label="New")
-fileMenu.add_separator()
-fileMenu.add_command(label="Exit", command=_quit)
-menuBar.add_cascade(label="File", menu=fileMenu)
-
-# Display a message box
-# Callback function
-def _msgBox():
-	# mBox.showinfo('Python Message Info Box', 'A Python GUI created using tkinter: \nThe Year is 2016.')
-	# Warning box
-	# mBox.showwarning('Python Message Warning Box', 'A Python GUI created using tkinter: \nWarning: There might be a bug in this code.')
-	# Error box
-	# mBox.showerror('Python Message Warning Box', 'A Python GUI created using tkinter:\nError: Houston we DO have a serious PROBLEM!')
-    answer = mBox.askyesno("Python Message Dual Choice Box", "Are you sure you really wish to do this?")
-    print(answer)
-
-helpMenu = Menu(menuBar, tearoff=0)
-helpMenu.add_command(label="About", command=_msgBox)
-menuBar.add_cascade(label="Help", menu=helpMenu)
-
-# Change the main windows icon
-# win.iconbitmap(r'/usr/bin/python3/icon/path')
-
-win.mainloop()
+# =========================
+# Start GUI
+# =========================
+oop = OOP()
+oop.win.mainloop()
